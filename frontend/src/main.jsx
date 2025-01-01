14771fcd927e5cfeb9d94750b8987325f377b8a5
Shreya Kathe <shreyakathe2904@gmail.com>
2025-01-01 19:41:36 +0530

    .

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import { UserContextProvider } from "./context/UserContext.jsx";
 import { CourseContextProvider } from "./context/CourseContext.jsx";
 
-export const server = import.meta.env.VITE_API_URL || "http://localhost:3000";
+export const server = "http://localhost:3000";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
