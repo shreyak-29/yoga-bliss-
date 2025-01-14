7af6545c927cf8522a23ca85a12a6940e2c6a059
Shreya Kathe <shreyakathe2904@gmail.com>
2025-01-14 13:46:58 +0530

    Refactor server URL to use environment variable and add console logs for debugging in Dashboard

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,8 +4,8 @@ import App from "./App.jsx";
 import { UserContextProvider } from "./context/UserContext.jsx";
 import { CourseContextProvider } from "./context/CourseContext.jsx";
 
-export const server = "http://localhost:3000";
-
+//import from env VITE_API_URL  and VITE_API_URL
+export const server = import.meta.env.VITE_API_URL;
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <UserContextProvider>
